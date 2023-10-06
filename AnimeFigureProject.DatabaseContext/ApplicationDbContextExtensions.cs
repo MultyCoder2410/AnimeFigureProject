fee897d6ff8316f9c8ad32d745c12dc908883a12
MultyCoder2410 <tristanfranssen@gmail.com>
2023-10-06 21:48:00 +0200

    Created migrations for database and extension.

---
+++ AnimeFigureProject.DatabaseContext/ApplicationDbContextExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AnimeFigureProject.DatabaseContext
+{
+
+    public static class ApplicationDbContextExtensions
+    {
+
+        public static IServiceCollection AddNorthwindContext(this IServiceCollection services, string connectionString)
+        {
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString); 
+                options.LogTo(Console.WriteLine, new[] { Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.CommandExecuting });
+
+            });
+
+            return services;
+        }
+
+    }
+
+}
